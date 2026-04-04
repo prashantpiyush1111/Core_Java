5d9c7374c5e6e67ff13640d1a632eeb0b32dfdb2
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-04-04 20:36:47 +0530

    Data Type.java

--- DataType.java
+++ DataType.java
@@ -1,14 +1,21 @@
 public class DataType {
 	public static void main(String[] args) {
-		int num1=9;
+		int num  =9;
+        System.out.println(num);
 		byte by=127;
+        System.out.println(by);
 		short sh=558;
+        System.out.println(sh);
 		long l=5854l;
+        System.out.println(l);
 		
 		float f=5.8f;
+        System.out.println(f);
 		double d=5.8;
+        System.out.println(d);
 		
 		char c='k';
+        System.out.println(c);
 		
 		boolean b=true;
         System.out.println(b);
