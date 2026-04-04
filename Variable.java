6b069a175311c3c4fe9979c172bbe2980d824f63
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-04-04 20:15:07 +0530

    Variable.java

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+
+	public static void main(String[] args) {
+		System.out.print(3+5);
+		System.out.print(8+7);
+		//int num1=3;
+		//int num2=5;
+		//System.out.println(num1+num2);
+		int num1=3;
+		int num2=5;
+		int result=num1+num2;
+		System.out.println(result);
+	}
+}
