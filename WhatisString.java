f74d43167aa9770d8a5481e20f6920e388ce34a6
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-04-13 17:27:42 +0530

    dd basic String object creation and operations demonstration in WhatisString class

---
+++ WhatisString.java
@@ -0,0 +1,13 @@
+// A String is an object that represents a sequence of characters like "Hello", "Java", or "123".
+public class WhatisString {
+	public static void main(String[] args) 
+	{
+		String name=new String();
+		System.out.println(name);
+		System.out.println(name.hashCode());
+		System.out.println("hello "+name);
+		System.out.println(name.concat("reddy"));
+//      String name="Navin";
+		
+	}
+}
